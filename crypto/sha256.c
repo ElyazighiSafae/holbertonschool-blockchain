f57570672cfc9a5602afc4f04c096367d3b45065
Farrukh Akhrarov <farruh1996@gmail.com>
2020-09-10 13:22:13 -0700

    Change SHA256 function to use internal function

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -11,24 +11,7 @@ uint8_t *sha256(int8_t const *s,
 				size_t len,
 				uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-	EVP_MD_CTX *mdctx;
-	const EVP_MD *md;
-
 	if (!s || !digest)
 		return (NULL);
-	md = EVP_sha256();
-	if (!md)
-		return (NULL);
-	mdctx = EVP_MD_CTX_new();
-	if (!mdctx)
-		return (NULL);
-	if (!EVP_DigestInit_ex(mdctx, md, NULL))
-		return (NULL);
-	if (!EVP_DigestUpdate(mdctx, s, len))
-		return (NULL);
-	if (!EVP_DigestFinal_ex(mdctx, digest, NULL))
-		return (NULL);
-	EVP_MD_CTX_free(mdctx);
-
-	return (digest);
+	return (SHA256((void *)s, len, digest));
 }
