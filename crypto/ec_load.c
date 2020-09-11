d0ff219ae5e4fa996768aacb71e6fd295f841d8d
Farrukh Akhrarov <farruh1996@gmail.com>
2020-09-10 17:17:49 -0700

    Add check if folder exists

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -10,9 +10,12 @@ EC_KEY *ec_load(char const *folder)
 	EC_KEY *key = NULL;
 	char file[512] = {0};
 	FILE *fp;
+	struct stat st;
 
 	if (!folder)
 		return (NULL);
+	if (stat(folder, &st) == -1)
+			return (NULL);
 	sprintf(file, "%s/%s", folder, PRI_FILENAME);
 	fp = fopen(file, "w");
 	if (!fp)
