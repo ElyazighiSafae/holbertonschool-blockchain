68f026629aeb4d332025132c82239a7186ef0b91
Farrukh Akhrarov <farruh1996@gmail.com>
2020-09-23 12:45:56 -0700

    Change malloc to calloc

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -16,19 +16,15 @@ block_t *block_create(block_t const *prev, int8_t const *data,
 
 	if (!prev || !data)
 		return (NULL);
-	block = malloc(sizeof(*block));
+	block = calloc(1, sizeof(*block));
 	if (!block)
 		return (NULL);
 
 	memcpy(block->data.buffer, data, max_len);
 	block->data.len = max_len;
 
-	memset(block->hash, 0, 32);
-
 	memcpy(block->info.prev_hash, prev->hash, 32);
 	block->info.index = prev->info.index + 1;
 	block->info.timestamp = (uint64_t)time(NULL);
-	block->info.difficulty = 0;
-	block->info.nonce = 0;
 	return (block);
 }
