6db2b9f6fd5236f85f773b64cd32ae54edb34685
Farrukh Akhrarov <farruh1996@gmail.com>
2020-10-22 22:14:46 -0700

    Fix compilation warnings

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -11,8 +11,6 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 				 size_t msglen, sig_t *sig)
 {
-	uint8_t digest[SHA256_DIGEST_LENGTH];
-
 	if (!key || !msg || !sig)
 		return (NULL);
 	if (!EC_KEY_check_key(key))
@@ -20,7 +18,7 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 	sig->len = (uint8_t)ECDSA_size(key);
 	if (!sig->len)
 		return (NULL);
-	if (!ECDSA_sign(EC_CURVE, msg, msglen,
+	if (!ECDSA_sign(EC_CURVE, msg, (int)msglen,
 					sig->sig, (uint32_t *)&(sig->len), (EC_KEY *)key))
 		return (NULL);
 	return (sig->sig);
