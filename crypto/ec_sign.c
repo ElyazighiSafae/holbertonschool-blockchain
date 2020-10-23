208695efbff7646df6d15a03605fd5bb5cac0e76
Farrukh Akhrarov <farruh1996@gmail.com>
2020-10-22 22:12:47 -0700

    Sign msg directly

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -17,12 +17,10 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 		return (NULL);
 	if (!EC_KEY_check_key(key))
 		return (NULL);
-	if (!SHA256(msg, msglen, digest))
-		return (NULL);
 	sig->len = (uint8_t)ECDSA_size(key);
 	if (!sig->len)
 		return (NULL);
-	if (!ECDSA_sign(EC_CURVE, digest, SHA256_DIGEST_LENGTH,
+	if (!ECDSA_sign(EC_CURVE, msg, msglen,
 					sig->sig, (uint32_t *)&(sig->len), (EC_KEY *)key))
 		return (NULL);
 	return (sig->sig);
