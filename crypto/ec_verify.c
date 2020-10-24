e8bc04f9dff39da2834185a0e60abbafce411f76
Farrukh Akhrarov <farruh1996@gmail.com>
2020-10-24 16:23:41 -0700

    Verify msg itself, not digest

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -11,15 +11,9 @@
 int ec_verify(EC_KEY const *key, uint8_t const *msg,
 				 size_t msglen, sig_t const *sig)
 {
-	uint8_t digest[SHA256_DIGEST_LENGTH];
-
 	if (!key || !msg || !sig)
 		return (0);
-	if (!EC_KEY_check_key(key))
-		return (0);
-	if (!SHA256(msg, msglen, digest))
-		return (0);
-	if (ECDSA_verify(0, digest, SHA256_DIGEST_LENGTH,
+	if (ECDSA_verify(0, msg, (int)msglen,
 					  sig->sig, sig->len, (EC_KEY *)key) < 1)
 		return (0);
 	return (1);
