fd3589eafdb6ac463a1809bfe0449c6b753ec22f
Farrukh Akhrarov <farruh1996@gmail.com>
2020-10-24 16:30:31 -0700

    Add task 4

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -1,6 +1,15 @@
 #ifndef __TRANSACTION_H_
 #define __TRANSACTION_H_
 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <llist.h>
+#include <openssl/sha.h>
+#include <time.h>
+#include <unistd.h>
 #include "../blockchain.h"
 
 #define ALIGN_SIZE(sizeToAlign, PowerOfTwo)       \
