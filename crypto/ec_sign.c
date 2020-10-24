5a5dbc1221136431c6b77832aed24dcf33e5a25b
Farrukh Akhrarov <farruh1996@gmail.com>
2020-10-24 16:07:51 -0700

    Remove extra check

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -16,8 +16,8 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 	if (!EC_KEY_check_key(key))
 		return (NULL);
 	/* sig->len = (uint8_t)ECDSA_size(key); */
-	if (!sig->len)
-		return (NULL);
+	/* if (!sig->len) */
+	/* 	return (NULL); */
 	if (!ECDSA_sign(EC_CURVE, msg, (int)msglen,
 					sig->sig, (uint32_t *)&(sig->len), (EC_KEY *)key))
 		return (NULL);
