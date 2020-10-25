ac749523f631cdee6e9605fa11936799e80169e6
Farrukh Akhrarov <farruh1996@gmail.com>
2020-10-25 15:17:23 -0700

    Update blockchain_create

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -19,7 +19,7 @@ block_t *block_create(block_t const *prev, int8_t const *data,
 	block = calloc(1, sizeof(*block));
 	if (!block)
 		return (NULL);
-	block->transactions = llist_create(MT_SUPPORT_FALSE);
+	block->transactions = llist_create(MT_SUPPORT_TRUE);
 	if (!block->transactions)
 		return (free(block), NULL);
 	memcpy(block->data.buffer, data, max_len);
