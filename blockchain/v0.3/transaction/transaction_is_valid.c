93f03a17151aa7d1466ed75b49a499eed0b08d50
Farrukh Akhrarov <farruh1996@gmail.com>
2020-10-25 14:14:33 -0700

    Add task 7

--- blockchain/v0.3/transaction/transaction_is_valid.c
+++ blockchain/v0.3/transaction/transaction_is_valid.c
@@ -29,6 +29,8 @@ int cmp_tr_ins(llist_node_t node, unsigned int idx, void *args)
 		*tx_in_idx += 1;
 		*balance += unspent->out.amount;
 		key = ec_from_pub(unspent->out.pub);
+		if (!key)
+			return (1);
 		if (!ec_verify(key, ptr[3], SHA256_DIGEST_LENGTH, &tx_in->sig))
 		{
 			EC_KEY_free(key);
